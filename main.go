6671839a3ce319ce74527e02792b4be0799bcff0
Raf Rucinski <rafal_rucinski@poczta.fm>
2022-07-26 22:50:37 +0100

    ok

--- main.go
+++ main.go
@@ -3,8 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var confName string = "RRCONF"
-	fmt.Printf("Welcome to our conf %v", confName)
+	var confName = "RRCONF"
+	const confTickets = 50
+	remainingTickets := 50
+	var userName string
+	var userTickets uint
 
-	
+	fmt.Printf("Welcome to our conf %v \n", confName)
+	fmt.Println("We have a total of", confTickets, "and", remainingTickets, "still remaining")
+
+	fmt.Println("What's your name?")
+	fmt.Scanln(&userName)
+	fmt.Println("Howmany tickets do you need?")
+	fmt.Scanln(&userTickets)
+	fmt.Println("Welcome", userName, "to",confName,"you've booked", userTickets, "tickets")
 }
