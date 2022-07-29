39ce5b82a293513af675685b42bd49315d9c389e
Raf Rucinski <rafal_rucinski@poczta.fm>
2022-07-29 17:55:50 +0100

    slices and for

--- main.go
+++ main.go
@@ -1,20 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
 
 func main() {
+
 	var confName = "RRCONF"
 	const confTickets = 50
 	remainingTickets := 50
-	var userName string
+	var firstName string
+	var lastName string
 	var userTickets uint
+	var bookings []string
+	for {
+			fmt.Printf("Welcome to our conf %v \n", confName)
+			fmt.Println("We have a total of", confTickets, "and", remainingTickets, "still remaining")
 
-	fmt.Printf("Welcome to our conf %v \n", confName)
-	fmt.Println("We have a total of", confTickets, "and", remainingTickets, "still remaining")
+			fmt.Println("What's your first name?")
+			fmt.Scanln(&firstName)
+			fmt.Println("What's your last name?")
+			fmt.Scanln(&lastName)
+			fmt.Println("Howmany tickets do you need?")
+			fmt.Scanln(&userTickets)
+			remainingTickets -= int(userTickets)
 
-	fmt.Println("What's your name?")
-	fmt.Scanln(&userName)
-	fmt.Println("Howmany tickets do you need?")
-	fmt.Scanln(&userTickets)
-	fmt.Println("Welcome", userName, "to",confName,"you've booked", userTickets, "tickets")
+			bookings = append(bookings, firstName + " " + lastName)
+
+			fmt.Println("Welcome", firstName, lastName, "to",confName,"you've booked", userTickets, "tickets. Remaining tickets:",
+							 remainingTickets,reflect.TypeOf(bookings), len(bookings), ". Enjoy", bookings[0])
+
+			fmt.Printf("Slice items: %v \n", bookings)
+
+			for index, booking := range bookings{
+				firstNameOnly := strings.Fields(booking)
+				fmt.Printf("slice element %v is %v \n", index, firstNameOnly[0])
+			}
+		}
+
 }
